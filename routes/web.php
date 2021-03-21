799c3990b25e9c5173bac318e3999f217c504ae5
bhushanshivle <55777860+bhushanshivle@users.noreply.github.com>
2021-03-21 16:20:33 +0530

    Recruite CRM assignment

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,11 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-//$router->group(['middleware'=>'auth:api'], function($router){
-$router->group(['prefix'=>'v1'], function($router){
+
+$router->group(['prefix'=>'v1', 'middleware' =>'http'], function($router){
     $router->get('login','UserController@authenticate');
     $router->get('candidates', 'CandidateController@showAllCandidates');
     $router->get('candidates/search', 'CandidateController@searchCandidates');
     $router->get('candidates/{id}', 'CandidateController@showCandidateById');
     $router->post('candidates', 'CandidateController@createCandidate');
 });
-//});
